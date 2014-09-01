9ace991bd9ce4142f4d97a75babe018ee4d5cc5e
Jonathan Coralo <jonathan.corallo92@gmail.com>
2014-09-01 03:38:44 -0300

    Reordenando

--- test/prototyped_spec.rb
+++ test/prototyped_spec.rb
@@ -3,21 +3,16 @@ require_relative '../src/PrototypedObject'
 
 describe 'Test de prototypes objects' do
 
-  it 'Deberia crear un atributo dinamicamente para una instancia particular' do
-    guerrero = PrototypedObject.new
-    guerrero.set_property(:energia, 100)
-    expect(guerrero.energia).to eq(100)
-  end
-
-  it 'Un atributo definido para una instancia no debe ser definida para otra' do
+  it 'Deberia definirse un atributo dinamicamente para una instancia unica' do
     guerrero = PrototypedObject.new
     otro_guerrero = PrototypedObject.new
     guerrero.set_property(:energia, 100)
 
+    expect(guerrero.energia).to eq(100)
     expect(otro_guerrero).not_to respond_to(:energia)
   end
 
-  it 'Deberia crear un metodo dinamicamente para una instancia particular' do
+  it 'Deberia crear un metodo dinamicamente para una instancia unica' do
     guerrero = PrototypedObject.new
     guerrero.set_property(:energia, 100)
     guerrero.set_property(:potencial_defensivo, 50)
@@ -33,6 +28,8 @@ describe 'Test de prototypes objects' do
         proc { |diferencia|
             self.energia = self.energia - diferencia
         })
+    otro_guerrero = PrototypedObject.new
+    expect(otro_guerrero).not_to respond_to(:atacar_a) #verificamos que el metodo solo lo pueda entender guerrero y no otras instancias
     otro_guerrero = guerrero.clone #clone es un metodo que ya viene definido en Ruby
     guerrero.atacar_a otro_guerrero
     expect(otro_guerrero.energia).to eq(80)
