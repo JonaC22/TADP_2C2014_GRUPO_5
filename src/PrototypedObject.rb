d1321ce03ed17fd65aaec7923ef5c0c681d9da64
Jonathan Coralo <jonathan.corallo92@gmail.com>
2014-09-01 03:30:56 -0300

    Agregado set_method

--- src/PrototypedObject.rb
+++ src/PrototypedObject.rb
@@ -1,6 +1,10 @@
 class PrototypedObject
-  def set_property symbol, valor
-      instance_variable_set("@#{symbol}", valor) #define el atributo y le setea el valor
-      self.singleton_class.send(:attr_accessor, symbol) #crea los getters y setters del atributo para solo la instancia del objeto
+  def set_property nombre_atributo, valor
+    instance_variable_set("@#{nombre_atributo}", valor) #define el atributo y le setea el valor
+    self.singleton_class.send(:attr_accessor, nombre_atributo) #crea los getters y setters del atributo para solo la instancia del objeto
+  end
+
+  def set_method nombre_metodo, accion
+    define_singleton_method(nombre_metodo,accion)
   end
 end
--- test/prototyped_spec.rb
+++ test/prototyped_spec.rb
@@ -3,7 +3,7 @@ require_relative '../src/PrototypedObject'
 
 describe 'Test de prototypes objects' do
 
-  it 'Deberia crear un atributo dinamicamente a un objeto y obtener su valor' do
+  it 'Deberia crear un atributo dinamicamente para una instancia particular' do
     guerrero = PrototypedObject.new
     guerrero.set_property(:energia, 100)
     expect(guerrero.energia).to eq(100)
@@ -17,4 +17,24 @@ describe 'Test de prototypes objects' do
     expect(otro_guerrero).not_to respond_to(:energia)
   end
 
+  it 'Deberia crear un metodo dinamicamente para una instancia particular' do
+    guerrero = PrototypedObject.new
+    guerrero.set_property(:energia, 100)
+    guerrero.set_property(:potencial_defensivo, 50)
+    guerrero.set_property(:potencial_ofensivo, 70)
+    guerrero.set_method(
+        :atacar_a,
+        proc { |otro_guerrero|
+          if (otro_guerrero.potencial_defensivo < self.potencial_ofensivo)
+            otro_guerrero.recibe_danio(self.potencial_ofensivo - otro_guerrero.potencial_defensivo)
+          end
+        });
+    guerrero.set_method(:recibe_danio,
+        proc { |diferencia|
+            self.energia = self.energia - diferencia
+        })
+    otro_guerrero = guerrero.clone #clone es un metodo que ya viene definido en Ruby
+    guerrero.atacar_a otro_guerrero
+    expect(otro_guerrero.energia).to eq(80)
+  end
 end
