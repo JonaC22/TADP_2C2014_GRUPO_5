b7a24f19c7e9ebbd175e60b7bb2c1af8b68ce5f6
Jonathan Coralo <jonathan.corallo92@gmail.com>
2014-10-23 17:11:29 -0300

    Validar destino transporte

--- TP2_Scala/ArgentinaExpress/src/tadp_grupo5/Transporte.scala
+++ TP2_Scala/ArgentinaExpress/src/tadp_grupo5/Transporte.scala
@@ -9,8 +9,20 @@ class Transporte(volumen : Int, costo : Int, velocidad: Int){
   
   def asignarPaquetes(nuevosPaquetes : Seq[Paquete]) : Unit = {
     if(capacidad < nuevosPaquetes.map(_.volumen).sum) throw new TransporteSinCapacidad()
-    else pedidos = pedidos ++ nuevosPaquetes
+    else {
+      validarDestinoPaquetes(nuevosPaquetes)
+      pedidos = pedidos ++ nuevosPaquetes
+    }
   }
+  
+  def validarDestinoPaquetes(paquetes : Seq[Paquete]) : Unit = {
+    var destino = paquetes.head.sucursalDestino
+    if(pedidos.size != 0) destino = pedidos.head.sucursalDestino
+    else {
+      if(!paquetes.forall(x => x.sucursalDestino == destino)) throw new PaquetesDestinoErroneo()
+    }
+  }
+  
 }
 
 case class Camion() extends Transporte(45, 100, 60)
@@ -19,4 +31,5 @@ case class Avion() extends Transporte(200,500,500){
   //debe tirar una excepcion si es utilizado para distancias menor o igual a 1000km
 }
 
+case class PaquetesDestinoErroneo() extends Exception
 case class TransporteSinCapacidad() extends Exception
