7b062e233bd1fcdd08b19982cb9a877cf634f806
Julian Zelayeta <julian.zelayeta@gmail.com>
2014-11-18 15:14:05 -0300

    refactor a funcional

--- TP2_Scala/Objeto-Funcional/src/tadp_grupo5/Sucursal.scala
+++ TP2_Scala/Objeto-Funcional/src/tadp_grupo5/Sucursal.scala
@@ -2,23 +2,31 @@ package tadp_grupo5
 
 import scala.collection.mutable.Buffer
 
-class Sucursal (volumenDeposito : Int, val pais : String) {
-  var paquetesEnSalir : Buffer[Paquete] = Buffer()
-  var paquetesEnEntrar : Buffer[Paquete] = Buffer()
-  var transportes : List[Transporte] = List()
+case class Sucursal (
+    volumenDeposito : Int,
+    pais : String)
+{
+  var paquetesEnSalir : List[Paquete] 
+  var paquetesEnEntrar : List[Paquete]
+  var transportes : List[Transporte]
   
   def capacidad : Int = volumenDeposito - paquetesEnEntrar.map(_.volumen).sum - paquetesEnSalir.map(_.volumen).sum  
   
   def esCasaCentral: Boolean = false
   
-  def asignarPaquete(paquete : Paquete) {
-    if(transportes.size != 0){
-      var transporte : Option[Transporte] = transportes.find( x => x.puedeLlevarPaquete(paquete))
-      transporte.get.asignarPaquete(paquete)
-    }
+  val transportesQuePuedenLLevar :Paquete => List[Transporte] = {
+    paquete =>
+      for{
+       transporte <- transportes if transporte.puedeLlevarPaquete(paquete)
+      }yield transporte
   }
-  
-  def paquetesPendientes : Buffer[Paquete] = paquetesEnSalir.filterNot(x => transportes.exists(_.pedidos.contains(x)))
+    
+  val asignarPaquete =  (paquete:Paquete) =>  transportesQuePuedenLLevar(paquete).head 
+ 
+  val paquetesPendientes  = 
+      for{
+        paquete <- paquetesEnSalir if transportes.exists(_.pedidos.contains(paquete))
+      }yield paquete
   
   def asignarPendientes(){
     var paquetes = paquetesPendientes
@@ -27,32 +35,36 @@ class Sucursal (volumenDeposito : Int, val pais : String) {
   
   def notificarPaqueteAEntrar(paquete : Paquete) {
     validarCapacidad(paquete)
-    paquetesEnEntrar += paquete
+    agregarPaquete(paquete,paquetesEnEntrar)  
   }
+  
+  val agregarPaquete = (_:Paquete)::(_:List[Paquete]) //Esto por ahora no sirve para nada...
+  
   
   def notificarPaqueteASalir(paquete : Paquete) {
     validarCapacidad(paquete)
-    paquetesEnSalir += paquete
+    agregarPaquete(paquete,paquetesEnSalir)
     asignarPaquete(paquete)
     asignarPendientes
   } 
+ 
+   def validarCapacidad(paquete : Paquete) = 
+     if (capacidad < paquete.volumen) throw new SucursalSinCapacidad()
   
-  def descargarEnvios(pedidos : Buffer[Paquete]){
-    pedidos.foreach(x => descargarEnvio(x))
-  }
+  val descargarEnvios = (pedidos:List[Paquete]) => for (pedido <- pedidos) descargarEnvio(pedido)
   
   def descargarEnvio(pedido : Paquete){
     if(pedido.sucursalDestino  == this){
-      paquetesEnEntrar = paquetesEnEntrar.filterNot(_== pedido)
+      paquetesEnEntrar = paquetesEnEntrar.filterNot(_== pedido) 
     }
-    else paquetesEnSalir = paquetesEnSalir.filterNot(_== pedido)
-  }
+    if (capacidad < pedido.volumen) throw new SucursalSinCapacidad()
+   }
 
-  def validarCapacidad(paquete : Paquete) = if (capacidad < paquete.volumen) throw new SucursalSinCapacidad()
 }
 
-case class CasaCentral(volumenDeposito : Int, override val pais : String) extends Sucursal(volumenDeposito, pais){
+class CasaCentral(volumenDeposito : Int, override val pais : String) extends Sucursal(volumenDeposito, pais){
   override def esCasaCentral : Boolean = true
 }
 
 case class SucursalSinCapacidad() extends Exception
+
